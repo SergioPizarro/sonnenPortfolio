142f895b1c9da93b5c06d53459815e1af97cbb79
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-10-20 13:02:29 +0200

    produkte ready

--- app/controllers/produkte_controller.rb
+++ app/controllers/produkte_controller.rb
@@ -7,4 +7,14 @@ class ProdukteController < ApplicationController
   def show
     @produkte = Produkte.find(params[:id])
   end
+
+
+  
 end
+
+
+#Produkte.create(tittle:"Pakete", description:"Energielösungen für Ihr Zuhause.")
+#Produkte.create(tittle:"Hardware", description:"Stromspeicher und Notstromlösungen.")
+#Produkte.create(tittle:"Stromtarife", description:"Saubere Energie - mit und ohne PV-Anlage. ")
+#Produkte.create(tittle:"E-Mobilität", description:"Endlich sauber Autofahren und tanken.")
+#Produkte.create(tittle:"Flexibilität", description:"Flexibel in Ihre Energiezukunft investieren.")
