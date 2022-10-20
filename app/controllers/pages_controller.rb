3601e818eada9910b37e66b9fa0eab327b89b4ba
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-10-20 02:51:01 +0200

    a lot done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,26 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def produkte
+
+  end
+
+  def sonnenCommunity
+
+  end
+
+  def wissen
+
+  end
+
+  def kontakt
+
+  end
+
+  def stromspeicher
+
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
-
+  get "/produkte", to:"pages#produkte", as: :produkte
+  get "/sonnenCommunity", to:"pages#sonnenComunity", as: :sonnenComunity
+  get "/wissen", to:"pages#wissen", as: :wissen
+  get "/kontact", to: "pages#kontact", as: :kontact
+  get "/stromspeicher", to: "pages#stromspeicher", as: :stromspeicher
 end
